2be142427c2e71114aecc1848fb1f89e32ea4890
Luiz Felipe <lfar05071@gmail.com>
2022-09-27 16:24:26 -0300

    commit imgs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponentComponent } from './components/test-component/test-component.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponentComponent
+    TestComponentComponent,
+    HeaderComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
